491c99475adc9b70b3bb7d56729bdbf31ce5fa2b
KaveeshKumar <28Kaveesh@gmail.com>
2022-02-28 17:49:31 +0530

    ExceptionHandling is Done

---
+++ Assignment1/Arithmetic.java
@@ -0,0 +1,20 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a,b,c ;
+		a = 4;
+		b= 0;
+	try {
+		   c= a/b;
+		   System.out.println(c);
+	}
+	catch (ArithmeticException e) {
+	 
+	 System.out.println("ArithmeticException Handled");	
+		
+
+
+	}
+	}
+}
---
+++ Assignment3/SavingAccount2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class SavingAccount2 {
+    public static void main(String[] args)  {
+        
+         double bal = 500 , withdraw = -500;
+         
+        
+        try{
+        
+        if(withdraw < 0)
+            throw new IllegalBankTransaction (withdraw-bal);
+            
+        }
+        
+        catch(IllegalBankTransaction e){
+            System.out.println("Illegal Bank Transaction");
+        }
+            
+        }
+            
+public static class IllegalBankTransaction extends Exception{
+    double amount;
+    IllegalBankTransaction(double amt){
+        amount = amt;
+    }
+}
+        
+}
